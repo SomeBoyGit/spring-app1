1109fca4a96de40c36c54d277552b91e9faf1386
someboygit <someboygit@gmail.com>
2022-08-12 22:20:54 +0300

    lesson7

--- src/main/java/ru/someboy/springcourse/MusicPlayer.java
+++ src/main/java/ru/someboy/springcourse/MusicPlayer.java
@@ -1,41 +1,39 @@
 package ru.someboy.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * @author Slipets Artem
  */
+@Component
 public class MusicPlayer {
-    private Music music;
-    private String name;
-    private int volume;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-    //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public MusicPlayer() {}
+    //    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+//
+//    @Autowired
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+//    public void playMusic() {
+//        System.out.println("Playing: " + music.getSong());
+//    }
+//    public void playMusic() {
+//        System.out.println("Playing: " + classicalMusic.getSong());
+//        System.out.println("Playing: " + rockMusic.getSong());
+//    }
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong() + " Playing: " + rockMusic.getSong();
     }
 }
--- src/main/java/ru/someboy/springcourse/TestSpring.java
+++ src/main/java/ru/someboy/springcourse/TestSpring.java
@@ -11,12 +11,16 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-        musicPlayer2.playMusic();
+//        Music music = context.getBean("rockMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+//        musicPlayer2.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
